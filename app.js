c8e821b39b3c3e7b8c64ae1d8649abec7a0394ab
sunduszaki <anaya.khan715715@gmail.com>
2022-05-28 21:12:32 +0500

    completed

---
+++ app.js
@@ -0,0 +1,570 @@
+//  //----Chapter 1----
+
+//  //Task no 1
+
+//  alert("ASSLAM O ALAIKUM TO ALL VISIOTRS")
+
+//  //Task no 2
+
+//  alert("Error! Please enter a valid password")
+
+// //Task no 3
+
+// alert('Welcome To JS land \n Happy coding')
+
+// //Task no 4 
+// alert ("Welcome To JS land")
+// alert("Happy coding")
+
+// //Task no 5 
+// console.log("Hello..I can run JS through my web browser's console")
+
+
+//----Chapter 2-----
+//Task no 1
+
+// var username ;
+// //Task no 2
+// var myName = "Sundus Zaki";
+// //Task no 3
+// var message = "Hello World"
+// alert(message)
+// // //Task no 4 
+// var name = "Sundus Zaki"
+// alert(name)
+// var age= "15"
+// alert(age)
+// var course = "Certified Mobile Application Development"
+// alert(course)
+// //Task no 5 
+
+// var a = "Pizza \n Pizz \n Piz \n Pi \n P";
+// alert(a);
+
+// //task no 6 
+// var email = "anaya.khan715715@gmail.com";
+// alert ("My email address is "+ email)
+
+// //task no 7 
+// var book = "A smarter way to learn Javascrpit" 
+// alert ("I am trying to learn from the Book " + book)
+
+//Task no 8 
+
+//Task no 9
+
+
+//----Chapter no 3 -----
+
+//Task no 1
+
+// var age = "15"
+// alert ("I am " + age + " years old")
+
+// //Task no 2 
+// var a = "14"
+// alert ("You have visited this site " + a  + " times")
+
+// //Task no 3
+// var birthyear ="1990";
+
+// document.getElementById("heading").innerHTML = "My birthyear is " + birthyear;
+
+
+
+// //Task no 4
+// var nama = "John Doe";
+// var product = "T-shirts";
+// var quantity = "5";
+// document.getElementById("heading").innerText = name + " ordered " + quantity + "  " + product + " on XYZ clothing store";
+
+
+
+//Chapter no 4
+//Task no 2
+
+//Legal variable Names
+
+// var name = "Sundus Zaki";
+// var num1 = 123;
+// var student_name = "Sundus Zaki";
+// var firstName = "Sundus Zaki";
+// var name$ = "Sundus Zaki";
+
+
+// //Ilegal Varibale Names 
+
+// var alert = "Sara Khan";
+// var 1 = "Sara Khan";
+// var "a"  = "Sara khan";
+// var Name = "Sara Khan";
+// var student name = "Sara Khan";
+
+// //Task no 3
+// var rules = "Rules for naming JS variables";
+
+// document.getElementById("heading").innerText = rules;
+
+// var paragraph = "A variable name can contain only letters, numbers, dollar signs, and underscores.For example $my_1stVaribale <br/> Variable must bring with a dollor,underscores and letters For example $name,_name and _name <br/> Variable name are case sensitive <br/> Variable name should not be JS keywords";
+
+// document.getElementById("paragraph").innerHTML = paragraph;
+
+
+
+
+
+//Chapter n 5
+
+//Task n 1
+
+// var a = 3+5;
+
+
+// document.getElementById("add").innerHTML = "Sum of 3 and 5 is " + a 
+
+// //Task n 2
+
+// var a = 3-5;
+
+
+// document.getElementById("sub").innerHTML = "Sum of 3 and 5 is " + a 
+
+// var a = 3*5;
+
+
+// document.getElementById("multiply").innerHTML = "Sum of 3 and 5 is " + a 
+
+// var a = 3/5;
+
+
+// document.getElementById("divide").innerHTML = "Sum of 3 and 5 is " + a 
+
+
+
+
+
+// //Task no 3
+
+// var a = "??";
+// document.getElementById("undefined").innerHTML = "value after variable declaration is : " + a ;
+
+
+// var a = 5;
+// document.getElementById("initial").innerHTML = "Initial value is : " + a; 
+
+
+// var a = 6;
+// document.getElementById("increment").innerHTML = "value after increment is : " + a; 
+
+
+// var a = 7+6;
+// document.getElementById("addition").innerHTML = "value after addition is : " + a ;
+
+
+
+// var a = 12;
+// document.getElementById("decrement").innerHTML = "value after decrement is : " + a ;
+
+// var a = 0;
+// document.getElementById("reminder").innerHTML = "The reminder  is : " + a ;
+
+
+
+
+
+// //Task no 4
+
+// var ticket = 600*5;
+// document.getElementById("ticket").innerText = "Total cost to buy 5 tickets to a movie is: " + ticket + "PKR";
+
+
+// //Task no 5
+// var rules = "Table of 4";
+
+//  document.getElementById("table").innerText = rules;
+
+
+//  var a = 4*1;
+
+
+// document.getElementById("one").innerHTML = "4 x 1 = " + a ;
+
+// var a = 4*2;
+
+
+// document.getElementById("two").innerHTML = "4 x 2 = " + a ;
+
+// var a = 4*3;
+
+
+// document.getElementById("three").innerHTML = "4 x 3 = " + a ;
+
+
+// var a = 4*4;
+
+
+// document.getElementById("four").innerHTML = "4 x 4 = " + a ;
+
+
+// var a = 4*5;
+
+
+// document.getElementById("five").innerHTML = "4 x 5 = " + a ;
+
+
+// var a = 4*6;
+
+
+// document.getElementById("six").innerHTML = "4 x 6 = " + a ;
+
+
+// var a = 4*7;
+
+
+// document.getElementById("seven").innerHTML = "4 x 7 = " + a ;
+
+
+// var a = 4*8;
+
+
+// document.getElementById("eight").innerHTML = "4 x 8 = " + a ;
+
+
+// var a = 4*9;
+
+
+// document.getElementById("nine").innerHTML = "4 x 9 = " + a ;
+
+
+// var a = 4*10;
+
+
+// document.getElementById("ten").innerHTML = "4 x 10 = " + a ;
+
+
+// //task no 6
+// var rules = "Shopping Order";
+
+//  document.getElementById("shopping").innerText = rules;
+
+// var a = 650;
+// document.getElementById("price").innerHTML = "Price of item 1  is : " + a ;
+
+// var a = 3;
+// document.getElementById("quantity").innerHTML = "Quantity of item 1 is : " + a ;
+
+// var a = 100;
+// document.getElementById("price1").innerHTML = "Price of item 2  is : " + a ;
+
+// var a = 7;
+// document.getElementById("quantity").innerHTML = "Quantity of item 2 is: " + a ;
+
+// var a = 100;
+// document.getElementById("charges").innerHTML = "Shipping Charges  : " + a ;
+
+// var a = 2750;
+// document.getElementById("cost").innerHTML = "Total cost of your order is : " + a ;
+
+
+
+// //Task no 7
+// var rules = "Marks Sheet";
+
+//  document.getElementById("marksheet").innerText = rules;
+
+//  var a = 980;
+// document.getElementById("marks").innerHTML = "Total marks : " + a ;
+
+// var a = 840;
+// document.getElementById("obtained").innerHTML = "Marks Obtained : " + a ;
+
+
+// var a = 82.04081;
+// document.getElementById("percentage").innerHTML = "Percentage : " + a ;
+
+
+
+//Chapter 6-9
+
+
+// var a = 10;
+
+// document.getElementById("value").innerHTML = "The value of a is: " + a ;
+
+
+// document.getElementById("eleven").innerHTML = "The value of ++a is: " + ++a ;
+
+// document.getElementById("Eleven").innerHTML = "Now the value of a is: " + a ;
+
+
+// document.getElementById("twelve").innerHTML = "The value of a++ is: " + a++ ;
+
+// document.getElementById("Twelve").innerHTML = "Now the value of a is: " + a ;
+
+
+// document.getElementById("subtract").innerHTML = "The value of --a is: " + --a ;
+
+// document.getElementById("Subtract").innerHTML = "Now the value of a is: " + a ;   
+
+
+// document.getElementById("zero").innerHTML = "The value of a-- is: " + a-- ;
+
+// document.getElementById("Zero").innerHTML = "Now the value of a is: " + a ;
+
+
+
+//Task no 2
+
+// var a = 2;
+// var b = 1;
+// document.write (--a - --b + ++b + b-- + b)
+
+//task no 3
+
+// var name = prompt ("What is your name?");
+// document.write ("welcome " + name);
+
+
+//task no 5
+// var num = prompt ("Enter a number", 5 );
+// document.write (num + " x 1 = " + num * 1 + "<br/>"+
+//  num + " x 2 = " + num * 2 +   "<br/>" +
+//   num + " x 3 = " + num * 3 +   "<br/>" +
+//   num + " x 4 = " + num * 4 +   "<br/>" +
+//   num + " x 5 = " + num * 5 +   "<br/>" +
+//   num + " x 6 = " + num * 6 +   "<br/>" +
+//   num + " x 7 = " + num * 7 +   "<br/>" +
+//   num + " x 8 = " + num * 8 +   "<br/>" +
+//   num + " x 9 = " + num * 9 +   "<br/>" +
+//   num + " x 10 = " + num * 10 +   "<br/>" );
+
+
+//task no 6
+var subject1 = prompt  ("Enter first subject");
+var subject2 = prompt  ("Enter second subject");
+var subject3 = prompt  ("Enter third subject");
+ 
+var marks1 = +prompt  ("Enter first marks");
+var marks2 = +prompt  ("Enter second marks");
+var marks3 = +prompt  ("Enter third marks");
+var marksAll = marks1 + marks2 + marks3 ;
+
+console.log (subject1);
+console.log (marks1);
+
+
+var percentage1 = marks1/ 100*100;
+console.log (percentage1);
+var percentage2 = marks2/ 100*100;
+console.log (percentage2);
+var percentage3 = marks3/ 100*100;
+console.log (percentage3);
+
+document.write("<table>");
+document.write("<tr>");
+//subject
+document.write("<th>");
+document.write("Subject");
+document.write("</th>");
+
+//total marks
+document.write("<th>");
+document.write("Total marks");
+document.write("</th>");
+
+//marks botained
+document.write("<th>");
+document.write("Obtained marks");
+document.write("</th>");
+
+//percentage
+document.write("<th>");
+document.write("Percentage");
+document.write("</th>");
+document.write("</tr>");
+
+
+//subject1
+document.write("<tr>");
+
+document.write("<td>");
+document.write(subject1);
+document.write("</td>");
+
+document.write("<td>");
+document.write(100);
+document.write("</td>");
+
+document.write("<td>");
+document.write(marks1);
+document.write("</td>");
+
+document.write("<td>");
+document.write(percentage1 + "%");
+document.write("</td>");
+
+document.write("</tr>");
+
+//subject2
+document.write("<tr>");
+
+document.write("<td>");
+document.write(subject2);
+document.write("</td>");
+
+document.write("<td>");
+document.write(100);
+document.write("</td>");
+
+document.write("<td>");
+document.write(marks2);
+document.write("</td>");
+
+document.write("<td>");
+document.write(percentage2 + "%");
+document.write("</td>");
+
+document.write("</tr>");
+
+
+
+//subject3
+document.write("<tr>");
+
+document.write("<td>");
+document.write(subject3);
+document.write("</td>");
+
+document.write("<td>");
+document.write(100);
+document.write("</td>");
+
+document.write("<td>");
+document.write(marks3);
+document.write("</td>");
+
+document.write("<td>");
+document.write(percentage3 + "%");
+document.write("</td>");
+
+document.write("</tr>");
+
+//total
+document.write("<tr>");
+document.write("<th>");
+document.write("</th>");
+
+
+document.write("<th>");
+document.write(300);
+
+document.write("</th>");
+
+document.write("<th>");
+document.write(marksAll);
+
+document.write("</th>");
+
+document.write("<th>");
+document.write(marksAll/300*100 + "%");
+
+
+document.write("</th>");
+
+document.write("</tr>");
+
+
+
+
+
+
+
+
+
+document.write("</table>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
